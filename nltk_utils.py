fae7ea45bd4f588cb231fc9fefd41f76c7a1fb7f
hmdalvin <63193506+hmdalvin@users.noreply.github.com>
2023-05-11 13:25:30 +0700

    Initial commit

---
+++ nltk_utils.py
@@ -0,0 +1,17 @@
+import nltk
+
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
+
+words = ["Organize", "organize", "organizing"]
+stemmed_words = [stem(w) for w in words]
+print(stemmed_words)
